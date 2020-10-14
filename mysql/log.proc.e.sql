af7bbe115614a251790458b0b8c4f6fe65c4ae89
Darbuly <xiaowei515@foxmail.com>
2020-10-14 15:16:40 +0800

    log.proc

--- mysql/log.proc.e.sql
+++ mysql/log.proc.e.sql
@@ -1,15 +1,5 @@
-DROP PROCEDURE IF EXISTS  `log.proc.e`;
-CREATE PROCEDURE `log.proc.e`(source_i VARCHAR(255),msg_i text)
-    BEGIN
-    DECLARE t_error INTEGER DEFAULT 0;
-    DECLARE CONTINUE HANDLER FOR SQLEXCEPTION SET t_error=1;
-
-        START TRANSACTION;
-            INSERT INTO proc_logs (type,source,message)  VALUES(0,source_i,msg_i);
-        IF t_error = 1 THEN
-            ROLLBACK;
-        ELSE
-            COMMIT;
-        END IF;
-   select t_error;  
+DROP PROCEDURE IF EXISTS  `log.proc`;
+CREATE PROCEDURE `log.proc`(source_i VARCHAR(255),msg_i text)
+begin
+     INSERT INTO proc_logs (type,source,message)  VALUES(0,source_i,msg_i);
 END;

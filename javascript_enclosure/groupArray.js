1f2e880326822cd8df76fdf4179db5327a9b1a19
Darbuly <xiaowei515@foxmail.com>
2021-05-11 18:43:24 +0800

    JS分组函数:

---
+++ javascript_enclosure/groupArray.js
@@ -0,0 +1,8 @@
+    function group(array, subGroupLength) {
+        let index = 0;
+        let newArray = [];
+        while(index < array.length) {
+            newArray.push(array.slice(index, index += subGroupLength));
+        }
+        return newArray;
+    }
